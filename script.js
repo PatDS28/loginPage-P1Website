0ea2d95dabcdc770b527783addd7a21b58c02b80
PatDS28 <johnpatrickdesalit@gmail.com>
2024-09-21 15:54:50 +0800

    signup complete

---
+++ script.js
@@ -0,0 +1,68 @@
+const form = document.getElementById('myForm');
+const firstname = document.getElementById('firstname-input');
+const email = document.getElementById('email-input');
+const password = document.getElementById('password-input');
+const repeatPassword = document.getElementById('repeat-password-input');
+const error_message = document.getElementById('error-message');
+
+form.addEventListener('submit', (e) => {
+  // console.log(firstname)
+  // prevent submit the form
+  e.preventDefault(); 
+  
+  let errors = []
+
+  if(firstname){
+    errors = getSignupFormErrors(firstname, email, password, repeatPassword);
+    console.log(errors);
+    console.log("asd")
+  }
+  else{
+    errors = getLoginFormErrors(email.value, password.value);
+    // console.log("asd")
+  }
+
+  if(errors.length > 0){
+    e.preventDefault();
+    error_message.innerText = errors.join(". ")
+  }
+})
+
+function getSignupFormErrors(firstname, email, password, repeatPassword){
+  let errors = [];
+  if(firstname.value === '' || firstname.value  == null){
+    errors.push('Firstname is required')
+    firstname.parentElement.classList.add('incorrect')
+  }
+  if(email.value  === '' || email.value  == null){
+    errors.push('Email is required')
+    email.parentElement.classList.add('incorrect')
+  }
+  if(password.value  === '' || password.value  == null){
+    errors.push('Password is required')
+    password.parentElement.classList.add('incorrect')
+  }
+  if(password.value.length < 8){
+    errors.push('Password must have at least 8 characters')
+    password.parentElement.classList.add('incorrect')
+  }
+  if(password.value !== repeatPassword.value){
+    errors.push('Password does not match')
+    password.parentElement.classList.add('incorrect')
+    repeatPassword.parentElement.classList.add('incorrect')
+  }
+
+  return errors;
+}
+
+const allInputs = [firstname, email, password, repeatPassword];
+
+allInputs.forEach(input => {
+  input.addEventListener('input', ()=>{
+    if(input.parentElement.classList.contains('incorrect')){
+      input.parentElement.classList.remove('incorrect');
+      error_message.innerText = '';
+    }
+  })
+})
+// firstname.parentElement.classList.add('incorrect')
